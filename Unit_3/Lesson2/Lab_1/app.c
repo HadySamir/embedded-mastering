56c9b57db982a1c6657c0d7c726a5452a342986c
HadySamir <121499482+HadySamir@users.noreply.github.com>
2023-02-27 13:08:19 +0200

    Delete Unit_3 directory

--- Unit_3/Lesson2/Lab_1/app.c
+++
@@ -1,6 +0,0 @@
-#include "uart.h"
-unsigned char str[100] = "learn-in-depth:<Hady>";
-void main(viod)
-{
-	uart_send_string(str);
-}
--- Unit_3/Lesson2/Lab_1/uart.h
+++
@@ -1,4 +0,0 @@
-#ifndef _UART_H
-#define _UART_H
-void uart_send_string(unsigned char* p);
-#endif
