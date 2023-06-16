6bde202e697c5e0629804196c8cce7063369de02
HadySamir <121499482+HadySamir@users.noreply.github.com>
2023-06-16 19:29:25 +0300

    Delete homework1. solution directory

--- homework1. solution/ex3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {  
-    float x,y,r ;
-    printf("Enter two floating numbers \n ");
-    scanf("%f %f", &x , &y);  
-    printf("%f * %f = %3.4f", x , y , r= x *y );
-    
-    return 0;
-}
--- homework1. solution/ex5.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {  
-    char x ;
-    printf("Enter a letter \n ");
-scanf("%c",&x); 
-    printf(" your letter ascii code = %d ", x  );
-    
-    return 0;
-}
